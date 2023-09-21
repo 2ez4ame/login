132248f0f5a71536d3c9e98149d4d95b2509927e
2ez4ame <116320047+2ez4ame@users.noreply.github.com>
2023-09-20 19:26:04 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <script defer src="js/bootstrap.bundle.min.js"></script>
+        <title></title>
+    </head>
+    <body style="background-image: url('assets/bg.gif')">
+    <div class="container">
+    <div class="row">
+      <div class="col-md-6 offset-md-3">
+       
+        
+        <div class="card my-5 ">
+          <form class="card-body cardbody-color p-lg-5 mb-0">
+
+            <div class="text-center">
+              <img src="assets/roel.p.jpg" class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3"
+                width="200px" alt="profile">
+            </div>
+  <div class="flex-container">
+  <div class="mb-3">
+              <input class="user-input" type="text" class="form-control" id="Username" aria-describedby="emailHelp"
+                placeholder="Username">
+            </div>
+            <div class="mb-3">
+              <input class="pass-input" type="password" class="form-control" id="password" placeholder="Password">
+            </div>
+  </div>
+ 
+            <div class="text-center"><button type="submit" class="login-btn">Login</button></div>
+            <div id="emailHelp" class="form-text text-center mb-5 text-white mt-4">Not
+              Registered? <a href="#" class="text-rgb(24, 119, 242) fw-bold"> Create an
+                Account</a>
+            </div>
+            <div class="in-footer-flex">
+              <a href="https://www.facebook.com/cedieg19"> <img class="icon" src="assets/fb.icon.png" alt="fb icon"></a>
+              <a href="https://www.instagram.com/bibiqueuexs/?hl=en"><img class="icon" src="assets/insta.png" alt="insta icon"></a>
+            <img class="icon1" src="assets/of.png" alt="of icon">
+
+          </div>
+          </form>
+ 
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <div>
+   
+  </div>
+
+  
+    </body>
+</html>
